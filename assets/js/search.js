376831e04e74ee8bec38fd6b2e892edd432a2135
judywawira <judywawira@users.noreply.github.com>
2020-08-07 01:36:39 +0000

    deploy: f5a417c69cb7dc4bf0c820026fa2b42a29ff6b3d

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/mogeni-and-gichoya-notes/assets/js/search-data.json', true);
+    request.open('GET', '/gichoya-notes/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
